2bab646eac1b11ed93958852a774ed4e27d70a66
Milena Khalil <mikhalil@f0r1s9.codam.nl>
2023-03-23 22:51:03 +0100

    dfjsfds

--- main2.c
+++ main2.c
@@ -1,78 +1,50 @@
-#include <stdio.h>
+// Written by Bruh
+
 #include <stdlib.h>
-#include <stdbool.h>
-#include "./MLX42/include/MLX42/MLX42.h"
+#include <stdio.h>
+#include <unistd.h>
+#include "MLX42/include/MLX42/MLX42.h"
+#define WIDTH 256
+#define HEIGHT 256
 
-#define WIDTH 1024
-#define HEIGHT 1024
-
-static mlx_image_t* image;
-
-int32_t ft_pixel(int32_t r, int32_t g, int32_t b, int32_t a)
+// Exit the program as failure.
+static void ft_error(void)
 {
-    return (r << 24 | g << 16 | b << 8 | a);
+	fprintf(stderr, "%s", mlx_strerror(mlx_errno));
+	exit(EXIT_FAILURE);
 }
 
-void ft_randomize(void* param)
+// Print the window width and height.
+static void ft_hook(void* param)
 {
-	for (int32_t i = 0; i < image->width; ++i)
-	{
-		for (int32_t y = 0; y < image->height; ++y)
-		{
-			uint32_t color = ft_pixel(
-				rand() % 0xFF, // R
-				rand() % 0xFF, // G
-				rand() % 0xFF, // B
-				rand() % 0xFF  // A
-			);
-			//mlx_put_pixel(image, i, y, color);
-			mlx_put_pixel(image, i, y, 2147483647);//100 << 24 | 150 << 16 | 16 << 8 | 200);
-		}
-	}
+	const mlx_t* mlx = param;
+
+//	printf("WIDTH: %d | HEIGHT: %d\n", mlx->width, mlx->height);
 }
 
-void ft_hook(void* param)                     // key pressing
+int32_t	main(void)
 {
-	mlx_t* mlx = param;
 
-	if (mlx_is_key_down(mlx, MLX_KEY_ESCAPE))
-		mlx_close_window(mlx);
-	if (mlx_is_key_down(mlx, MLX_KEY_UP))
-		image->instances[0].y -= 5;
-	if (mlx_is_key_down(mlx, MLX_KEY_DOWN))
-		image->instances[0].y += 5;
-	if (mlx_is_key_down(mlx, MLX_KEY_LEFT))
-		image->instances[0].x -= 5;
-	if (mlx_is_key_down(mlx, MLX_KEY_RIGHT))
-		image->instances[0].x += 5;
-}
+	// MLX allows you to define its core behaviour before startup.
+	mlx_set_setting(MLX_MAXIMIZED, true);
+	mlx_t* mlx = mlx_init(WIDTH, HEIGHT, "42Balls", true);
+	if (!mlx)
+		ft_error();
 
-int32_t main(int32_t argc, const char* argv[])
-{
-	mlx_t* mlx;
+	/* Do stuff */
 
-	// Gotta error check this stuff
-	if (!(mlx = mlx_init(WIDTH, HEIGHT, "MLX42", true)))
-	{
-		puts(mlx_strerror(mlx_errno));
-		return(EXIT_FAILURE);
-	}
-	if (!(image = mlx_new_image(mlx, 28, 128)))
-	{
-		mlx_close_window(mlx);
-		puts(mlx_strerror(mlx_errno));
-		return(EXIT_FAILURE);
-	}
-	if (mlx_image_to_window(mlx, image, 0, 0) == -1)
-	{
-		mlx_close_window(mlx);
-		puts(mlx_strerror(mlx_errno));
-		return(EXIT_FAILURE);
-	}
-	
-	mlx_loop_hook(mlx, ft_randomize, mlx);
+	// Create and display the image.
+	mlx_image_t* img = mlx_new_image(mlx, 256, 256);
+	if (!img || (mlx_image_to_window(mlx, img, 0, 0) < 0))
+		ft_error();
+
+	// Even after the image is being displayed, we can still modify the buffer.
+	mlx_put_pixel(img, 0, 0, 0xFF0000FF);
+
+	// Register a hook and pass mlx as an optional param.
+	// NOTE: Do this before calling mlx_loop!
+
 	mlx_loop_hook(mlx, ft_hook, mlx);
-
 	mlx_loop(mlx);
 	mlx_terminate(mlx);
 	return (EXIT_SUCCESS);
